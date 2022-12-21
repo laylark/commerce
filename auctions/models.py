ca5ddf71cfdd8f0693bc7e4d64c92775e31c08e7
laylark <laylan@gmail.com>
2022-12-20 21:24:22 -0600

    Create superuser and register models in admin.py

--- auctions/models.py
+++ auctions/models.py
@@ -4,8 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=160)
@@ -15,13 +20,22 @@ class Listing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings")
 
+    def __str__(self):
+        return f"{self.title}, {self.description}, {self.img_path}, {self.price}, {self.created_at}, {self.user}, {self.category}"
+
 class Bid(models.Model):
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"{self.amount}, {self.listing}, {self.user}"
 
 class Comment(models.Model):
     text = models.TextField()
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.text}, {self.listing}, {self.user}, {self.created_at}"
