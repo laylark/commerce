47e1e0c561c10eb0fa20b456b5890f4c335fdc1a
laylark <laylan@gmail.com>
2022-12-20 22:41:03 -0600

    Add Django form to create listings

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
@@ -16,7 +17,7 @@ class Listing(models.Model):
     description = models.CharField(max_length=160)
     img_path = models.CharField(max_length=1024)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings")
 
--- auctions/views.py
+++ auctions/views.py
@@ -1,18 +1,51 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django import forms
 
 from .models import User, Category, Listing, Bid, Comment
 
+# Create class for new listing form
+class NewListingForm(forms.Form):
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    description = forms.CharField(label="Description", widget=forms.Textarea(attrs={'class' : 'form-control'}))
+    image = forms.CharField(label="Image path", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    price = forms.DecimalField(label="Price", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    category = forms.ModelChoiceField(queryset=Category.objects.all().order_by('name'))
 
+# Render index page with all listings
 def index(request):
     return render(request, "auctions/index.html", {
-        "categories": Category.objects.all()
+        "listings": Listing.objects.all()
     })
 
+# Create a new listing 
+def create_listing(request):
+    if request.method == "POST":
+            # Create a new listing form
+            form = NewListingForm(request.POST)
 
+            # Validate form
+            if not form.is_valid():
+                # If not valid, render new listing page along with form data submitted by user
+                return render(request, "auctions/create.html", {"form": form})
+
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            image = form.cleaned_data["image"]
+            price = form.cleaned_data["price"]
+            category = form.cleaned_data["category"]
+
+            # Save listing to listing table and redirect to index page
+            listing = Listing(title=title, description=description, img_path=image, price=price, category=category, user=request.user)
+            listing.save()
+            return redirect("index")
+
+    # Render new listing form page
+    return render(request, "auctions/create.html", {"form": NewListingForm()})
+        
 def login_view(request):
     if request.method == "POST":
 
